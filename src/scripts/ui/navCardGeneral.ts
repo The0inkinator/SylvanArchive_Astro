dc61475405ecb1e2b2ee6535a9ed59dc4b53d911
TheOinkinator <theoinkinator@gmail.com>
2023-04-22 23:55:00 -0700

    add bookmark bar function

--- src/scripts/ui/navCardGeneral.ts
+++ src/scripts/ui/navCardGeneral.ts
@@ -1,5 +1,5 @@
 const pageNavCards = document.getElementsByClassName("navCardBookmark");
-const bookmarkedNavCards: string[] = [];
+export const bookmarkedNavCards: object[] = [];
 
 for (let i = 0; i < pageNavCards.length; i++) {
   pageNavCards[i].setAttribute("id", "pageNavCard" + i);
@@ -7,13 +7,23 @@ for (let i = 0; i < pageNavCards.length; i++) {
   pageNavCards[i].addEventListener("click", () => {
     let navCardClicked = false;
     const pageNavCard = document.getElementById(`pageNavCard${i}`);
+
     if (pageNavCard instanceof HTMLElement) {
+      const pageNavCardTitle = pageNavCard.previousSibling?.previousSibling
+        ?.previousSibling?.previousSibling as HTMLDivElement;
+      const pageNavCardImage = pageNavCard.previousSibling?.previousSibling
+        ?.previousSibling?.previousSibling?.previousSibling?.previousSibling
+        ?.previousSibling?.previousSibling as HTMLElement;
+
       if (navCardClicked === false) {
         pageNavCard.style.backgroundImage =
           "url('../../graphics/bookmarkOn.svg')";
         navCardClicked = true;
-        bookmarkedNavCards.push(`pageNavCard${i}`);
-        console.log(bookmarkedNavCards);
+        bookmarkedNavCards.push({
+          id: `pageNavCard${i}`,
+          title: pageNavCardTitle.innerHTML,
+          image: pageNavCardImage.style.backgroundImage,
+        });
       } else {
         pageNavCard.style.backgroundImage =
           "url('../../graphics/bookmarkOff.svg')";
