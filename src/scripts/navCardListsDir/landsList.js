a9147122b51b5fcba279668e028e0b55a970500d
TheOinkinator <theoinkinator@gmail.com>
2023-04-30 23:01:10 -0700

    add solid dependancy test solid ui elements

--- src/scripts/navCardListsDir/landsList.js
+++ src/scripts/navCardListsDir/landsList.js
@@ -5,6 +5,7 @@ export const landsListRaw = [
     imgSrc:
       "https://cards.scryfall.io/art_crop/front/8/c/8c2996d9-3287-4480-8c04-7a378e37e3cf.jpg?1571667973",
     link: "/",
+    bookmarked: false,
   },
   {
     title: "Utility",
@@ -12,5 +13,6 @@ export const landsListRaw = [
     imgSrc:
       "https://cards.scryfall.io/art_crop/front/d/5/d510eece-4f55-4198-83fe-5291a76ffdcc.jpg?1637114800",
     link: "/",
+    bookmarked: false,
   },
 ];
