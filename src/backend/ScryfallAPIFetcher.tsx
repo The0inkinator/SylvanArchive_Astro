8ec8221342d521c3e9c483454701dd4ce4d865d9
The0inkinator <127580789+The0inkinator@users.noreply.github.com>
2023-08-18 13:07:27 -0700

    update cardArtFetcherA

--- src/backend/ScryfallAPIFetcher.tsx
+++ src/backend/ScryfallAPIFetcher.tsx
@@ -1,4 +1,71 @@
-import { createSignal, createEffect } from "solid-js";
+import { createSignal, createEffect } from 'solid-js';
+
+export function CardArtFetcherA(
+  cardName: string,
+  cardSet: string | null,
+  cardCollectNum: string | null
+): Promise<string | null> {
+  return new Promise<string | null>(async (resolve) => {
+    const [cardArtUrl, setCardArtUrl] = createSignal<string | null>(null);
+
+    createEffect(async () => {
+      try {
+        const inputCardName = await fetch(
+          `https://api.scryfall.com/cards/named?exact=${encodeURIComponent(
+            cardName
+          )}`
+        );
+
+        const initCard = await inputCardName.json();
+
+        const cardListFetch = await fetch(
+          ` https://api.scryfall.com/cards/search?q=${initCard.name}%20unique%3Aprints
+          `
+        );
+
+        const cardList = await cardListFetch.json();
+
+        // console.log(
+        //   'Data returned by awaiting cardListFetch.json():',
+        //   cardList
+        // );
+
+        if (cardCollectNum) {
+          console.log('Collector number input:', cardCollectNum);
+        } else if (cardSet) {
+          console.log('Set code input:', cardSet);
+          // cardList.data.forEach((card: { set: string }, listPos: number) => {
+          //   if (card.set === cardSet) {
+          //     console.log('matched card set');
+          //   }
+          // });
+          const cardSetMatch = cardList.data.some(
+            (card: { set: string }, listPos: number) => {
+              if (card.set === cardSet) {
+                console.log('card set match found:', cardList.data[listPos]);
+                return true;
+              }
+              console.log('no match found');
+              return false;
+            }
+          );
+        } else {
+          if (initCard.image_uris && initCard.image_uris.art_crop) {
+            console.log('card art found:', initCard.image_uris.art_crop);
+
+            resolve(initCard.image_uris.art_crop);
+          }
+        }
+
+        resolve(
+          'https://cards.scryfall.io/art_crop/front/5/2/52558748-6893-4c72-a9e2-e87d31796b59.jpg?1559959349'
+        );
+      } catch (error) {
+        console.error(`Error fetching card image for "${cardName}"`, error);
+      }
+    });
+  });
+}
 
 export function CardArtFetcher(cardName: string): Promise<string | null> {
   return new Promise<string | null>(async (resolve) => {
@@ -19,7 +86,7 @@ export function CardArtFetcher(cardName: string): Promise<string | null> {
           resolve(data.image_uris.art_crop);
         } else {
           resolve(
-            "https://cards.scryfall.io/art_crop/front/5/2/52558748-6893-4c72-a9e2-e87d31796b59.jpg?1559959349"
+            'https://cards.scryfall.io/art_crop/front/5/2/52558748-6893-4c72-a9e2-e87d31796b59.jpg?1559959349'
           );
         }
       } catch (error) {
@@ -48,7 +115,7 @@ export function CardFetcher(cardName: string): Promise<string | null> {
           resolve(data.image_uris.normal);
         } else {
           resolve(
-            "https://cards.scryfall.io/png/front/5/2/52558748-6893-4c72-a9e2-e87d31796b59.png?1559959349"
+            'https://cards.scryfall.io/png/front/5/2/52558748-6893-4c72-a9e2-e87d31796b59.png?1559959349'
           );
         }
       } catch (error) {
--- src/components/APITester.tsx
+++ src/components/APITester.tsx
@@ -1,56 +1,36 @@
-import { CardArtFetcher } from "../backend/ScryfallAPIFetcher";
-import { createEffect, createSignal } from "solid-js";
+import { CardArtFetcher, CardArtFetcherA } from '../backend/ScryfallAPIFetcher';
+import { createEffect, createSignal } from 'solid-js';
 
-export default function APITester({
-  cardNameA,
-  cardNameB,
-}: {
-  cardNameA: string;
-  cardNameB: string;
-}) {
+export default function APITester() {
   const [cardArtUrl_A, setCardArtUrl_A] = createSignal<string | null>(null);
-  const [cardArtUrl_B, setCardArtUrl_B] = createSignal<string | null>(null);
-  const [cardArtUrl_C, setCardArtUrl_C] = createSignal<string | null>(null);
-  const [cardArtUrl_D, setCardArtUrl_D] = createSignal<string | null>(null);
   createEffect(async () => {
-    const url = await CardArtFetcher(cardNameA);
+    const url = await CardArtFetcherA('abundant harvest', 'sta', '');
     setCardArtUrl_A(url);
-  });
-
-  createEffect(async () => {
-    const url = await CardArtFetcher(cardNameB);
-    setCardArtUrl_B(url);
   });
 
   return (
     <>
-      <button
-        onClick={() => {
-          console.log(cardArtUrl_A());
-        }}
-      >
-        Run API Test
-      </button>
-      <div
-        style={{
-          "background-color": "red",
-          width: "12rem",
-          height: "12rem",
-          "background-image": cardArtUrl_B()
-            ? `url(${cardArtUrl_A()})`
-            : "none",
-        }}
-      ></div>
-      <div
-        style={{
-          "background-color": "red",
-          width: "12rem",
-          height: "12rem",
-          "background-image": cardArtUrl_B()
-            ? `url(${cardArtUrl_B()})`
-            : "none",
-        }}
-      ></div>
+      <div style={'display: flex; flex-direction: column'}>
+        <button
+          onClick={() => {
+            console.log(cardArtUrl_A());
+          }}
+        >
+          Run API Test
+        </button>
+        <div
+          style={{
+            'background-color': 'red',
+            width: '12rem',
+            height: '12rem',
+            'background-size': 'cover',
+            'background-position': 'center',
+            'background-image': cardArtUrl_A()
+              ? `url(${cardArtUrl_A()})`
+              : 'none',
+          }}
+        ></div>
+      </div>
     </>
   );
 }
