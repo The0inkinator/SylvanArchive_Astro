c033ae011430225a2972ad9db780eeac0833d325
TheOinkinator <theoinkinator@gmail.com>
2023-08-17 23:38:36 -0700

    add cardfetcher function to gridcard component

--- src/backend/ScryfallAPIFetcher.tsx
+++ src/backend/ScryfallAPIFetcher.tsx
@@ -7,7 +7,7 @@ export function CardArtFetcher(cardName: string): Promise<string | null> {
     createEffect(async () => {
       try {
         const response = await fetch(
-          `https://api.scryfall.com/cards/named?fuzzy=${encodeURIComponent(
+          `https://api.scryfall.com/cards/named?exact=${encodeURIComponent(
             cardName
           )}`
         );
@@ -24,9 +24,35 @@ export function CardArtFetcher(cardName: string): Promise<string | null> {
         }
       } catch (error) {
         console.error(`Error fetching card image for "${cardName}"`, error);
-        setCardArtUrl(
-          "https://cards.scryfall.io/art_crop/front/5/2/52558748-6893-4c72-a9e2-e87d31796b59.jpg?1559959349"
+      }
+    });
+  });
+}
+
+export function CardFetcher(cardName: string): Promise<string | null> {
+  return new Promise<string | null>(async (resolve) => {
+    const [cardUrl, setCardUrl] = createSignal<string | null>(null);
+
+    createEffect(async () => {
+      try {
+        const response = await fetch(
+          `https://api.scryfall.com/cards/named?exact=${encodeURIComponent(
+            cardName
+          )}`
         );
+
+        const data = await response.json();
+
+        if (data.image_uris && data.image_uris.normal) {
+          setCardUrl(data.image_uris.normal);
+          resolve(data.image_uris.normal);
+        } else {
+          resolve(
+            "https://cards.scryfall.io/png/front/5/2/52558748-6893-4c72-a9e2-e87d31796b59.png?1559959349"
+          );
+        }
+      } catch (error) {
+        console.error(`Error fetching card image for "${cardName}"`, error);
       }
     });
   });
--- src/components/Grid Card/GridCard.tsx
+++ src/components/Grid Card/GridCard.tsx
@@ -1,33 +1,87 @@
 import "./gridCardStyles.css";
 import "./popUpStyles.css";
 import { createSignal, createEffect, onMount } from "solid-js";
+import { CardArtFetcher, CardFetcher } from "../../backend/ScryfallAPIFetcher";
 
-const [gridCardHovered, setGridCardHovered] = createSignal<boolean>(false);
+export default function GridCard({
+  displayArt,
+  bgCard1,
+  bgCard2,
+  bgCard3,
+}: {
+  displayArt: string;
+  bgCard1: string;
+  bgCard2: string;
+  bgCard3: string;
+}) {
+  const [displayArtUrl, setDisplayArtUrl] = createSignal<string | null>(null);
+  const [bgCard1Url, setBgCard1Url] = createSignal<string | null>(null);
+  const [bgCard2Url, setBgCard2Url] = createSignal<string | null>(null);
+  const [bgCard3Url, setBgCard3Url] = createSignal<string | null>(null);
 
-export default function GridCard() {
+  createEffect(async () => {
+    const url = await CardArtFetcher(displayArt);
+    setDisplayArtUrl(url);
+  });
+
+  createEffect(async () => {
+    const url = await CardFetcher(bgCard1);
+    setBgCard1Url(url);
+  });
+
+  createEffect(async () => {
+    const url = await CardFetcher(bgCard2);
+    setBgCard2Url(url);
+  });
+
+  createEffect(async () => {
+    const url = await CardFetcher(bgCard3);
+    setBgCard3Url(url);
+  });
+
   return (
     <>
       <div class="gridCardContainer">
         <div class="fullGridCard" tabindex="0">
-          <div
-            class="gridCardBox"
-            onmouseenter={() => {
-              setGridCardHovered(true);
-            }}
-            onmouseleave={() => {
-              setGridCardHovered(false);
-            }}
-          >
-            <div class="gridCardImage"></div>
+          <div class="gridCardBox">
+            <div
+              class="gridCardImage"
+              style={{
+                "background-image": displayArtUrl()
+                  ? `url(${displayArtUrl()})`
+                  : "none",
+              }}
+            ></div>
             <div class="overlay"></div>
             <div class="gridCardTitle">Title</div>
             <a class="link"></a>
           </div>
 
           <div class="popUpContainer">
-            <div class="popUpCard"></div>
-            <div class="popUpCard"></div>
-            <div class="popUpCard"></div>
+            <div
+              class="popUpCard"
+              style={{
+                "background-image": bgCard1Url()
+                  ? `url(${bgCard1Url()})`
+                  : "none",
+              }}
+            ></div>
+            <div
+              class="popUpCard"
+              style={{
+                "background-image": bgCard2Url()
+                  ? `url(${bgCard2Url()})`
+                  : "none",
+              }}
+            ></div>
+            <div
+              class="popUpCard"
+              style={{
+                "background-image": bgCard3Url()
+                  ? `url(${bgCard3Url()})`
+                  : "none",
+              }}
+            ></div>
           </div>
         </div>
       </div>
