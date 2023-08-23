cc8dbe22ed7016c4a568de90ba3f1cfa23b070a3
The0inkinator <127580789+The0inkinator@users.noreply.github.com>
2023-08-23 09:29:53 -0700

    add base list structure for testing

---
+++ src/lists/index.tsx
@@ -0,0 +1,73 @@
+type CardFetcherInputs = {
+  cardName: string;
+  cardSet?: string;
+  cardCollectNum?: number;
+  cardFace?: 'front' | 'back';
+};
+
+interface GridCardInputs {
+  link: string;
+  title: string;
+  displayArt: CardFetcherInputs;
+  bgCards?: CardFetcherInputs[];
+}
+const FixingLands: GridCardInputs[] = [
+  {
+    title: 'Lands',
+    link: '',
+    displayArt: { cardName: 'Evolving Wilds', cardSet: 'bfz' },
+    bgCards: [{ cardName: 'City of Brass' }, { cardName: 'Rishadan Port' }],
+  },
+  {
+    title: 'Creatures',
+    link: '',
+    displayArt: { cardName: 'Morophon the boundless' },
+    bgCards: [
+      { cardName: 'Goblin Guide' },
+      { cardName: 'Llanowar Elves' },
+      { cardName: 'Mulldrifter' },
+    ],
+  },
+  {
+    title: 'Spells',
+    link: '',
+    displayArt: { cardName: 'Supreme Verdict', cardSet: '2x2' },
+    bgCards: [
+      { cardName: 'Vindicate' },
+      { cardName: 'Counterspell' },
+      { cardName: 'Giant Growth' },
+    ],
+  },
+  {
+    title: 'Archtypes',
+    link: '',
+    displayArt: { cardName: 'Mayhem Devil' },
+    bgCards: [
+      { cardName: 'Emry Lurker of the loch' },
+      { cardName: 'Expressive iteration' },
+      { cardName: 'Bloodghast' },
+    ],
+  },
+  {
+    title: 'Colors',
+    link: '',
+    displayArt: { cardName: 'Mana confluence', cardSet: 'sld' },
+    bgCards: [
+      { cardName: 'Swords to plowshares' },
+      { cardName: 'counterspell' },
+      { cardName: 'thoughtseize' },
+    ],
+  },
+];
+export default FixingLands;
+
+// {
+//   title: 'Title',
+//   link: '',
+//   displayArt: { cardName: 'DiplayArt'},
+//   bgCards: [
+//     { cardName: 'Bg Card 1' },
+//     { cardName: 'Bg Card  2' },
+//     { cardName: 'Bg Card  3' },
+//   ],
+// },
