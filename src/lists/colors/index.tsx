a3a4daa1315093741a7c1243f852e71217f144ff
TheOinkinator <theoinkinator@gmail.com>
2023-08-23 23:32:44 -0700

    tested smaller card

--- src/lists/colors/index.tsx
+++ src/lists/colors/index.tsx
@@ -12,56 +12,56 @@ interface GridCardInputs {
   bgCards?: CardFetcherInputs[];
 }
 const FixingLands: GridCardInputs[] = [
-  {
-    title: "White",
-    link: "",
-    displayArt: { cardName: "swords to plowshares", cardSet: "ema" },
-    bgCards: [
-      { cardName: "thraben inspector" },
-      { cardName: "thalia, guardian of thraben" },
-      { cardName: "armageddon" },
-    ],
-  },
-  {
-    title: "Blue",
-    link: "",
-    displayArt: { cardName: "counterspell", cardSet: "ema" },
-    bgCards: [
-      { cardName: "preordain" },
-      { cardName: "thassas oracle" },
-      { cardName: "as foretold" },
-    ],
-  },
-  {
-    title: "Black",
-    link: "",
-    displayArt: { cardName: "thoughtseize" },
-    bgCards: [
-      { cardName: "fatal push" },
-      { cardName: "dark confidant" },
-      { cardName: "necropotence" },
-    ],
-  },
-  {
-    title: "Red",
-    link: "",
-    displayArt: { cardName: "fiery emancipation" },
-    bgCards: [
-      { cardName: "goblin guide" },
-      { cardName: "searing blaze" },
-      { cardName: "chandra torch of defiance" },
-    ],
-  },
-  {
-    title: "Green",
-    link: "",
-    displayArt: { cardName: "craterhoof behemoth" },
-    bgCards: [
-      { cardName: "Elvish Mystic" },
-      { cardName: "farseek" },
-      { cardName: "Natural Order" },
-    ],
-  },
+  // {
+  //   title: "White",
+  //   link: "",
+  //   displayArt: { cardName: "swords to plowshares", cardSet: "ema" },
+  //   bgCards: [
+  //     { cardName: "thraben inspector" },
+  //     { cardName: "thalia, guardian of thraben" },
+  //     { cardName: "armageddon" },
+  //   ],
+  // },
+  // {
+  //   title: "Blue",
+  //   link: "",
+  //   displayArt: { cardName: "counterspell", cardSet: "ema" },
+  //   bgCards: [
+  //     { cardName: "preordain" },
+  //     { cardName: "thassas oracle" },
+  //     { cardName: "as foretold" },
+  //   ],
+  // },
+  // {
+  //   title: "Black",
+  //   link: "",
+  //   displayArt: { cardName: "thoughtseize" },
+  //   bgCards: [
+  //     { cardName: "fatal push" },
+  //     { cardName: "dark confidant" },
+  //     { cardName: "necropotence" },
+  //   ],
+  // },
+  // {
+  //   title: "Red",
+  //   link: "",
+  //   displayArt: { cardName: "fiery emancipation" },
+  //   bgCards: [
+  //     { cardName: "goblin guide" },
+  //     { cardName: "searing blaze" },
+  //     { cardName: "chandra torch of defiance" },
+  //   ],
+  // },
+  // {
+  //   title: "Green",
+  //   link: "",
+  //   displayArt: { cardName: "craterhoof behemoth" },
+  //   bgCards: [
+  //     { cardName: "Elvish Mystic" },
+  //     { cardName: "farseek" },
+  //     { cardName: "Natural Order" },
+  //   ],
+  // },
   {
     title: "White",
     link: "",
