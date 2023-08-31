cb0735fc2cdeab9364b5ad0bb9bbe2f1e713f03c
TheOinkinator <theoinkinator@gmail.com>
2023-08-31 15:08:05 -0700

    finalize shelfscene architecture

--- src/components/shelf/Shelf.tsx
+++ src/components/shelf/Shelf.tsx
@@ -1,8 +1,5 @@
-import './shelfStyles.css';
-import Stack from '../stack/Stack';
-// import { StackDraggingProvider } from '../../context/StackDraggingContext';
-// import { SelectedBinderProvider } from '../../context/SelectedBinderContext';
-// import { ActiveStackProvider } from '../../context/ActiveStackContext';
+import "./shelfStyles.css";
+import Stack from "../stack/Stack";
 
 interface ShelfInputs {
   shelfRef: string;
@@ -12,16 +9,10 @@ interface ShelfInputs {
 
 export default function Shelf({ shelfRef, shelfFrom, shelfTo }: ShelfInputs) {
   return (
-    // <SelectedBinderProvider selectedBinderState={0}>
-    //   <StackDraggingProvider dragState={'still'}>
-    //     <ActiveStackProvider activeStackState={null}>
-          <div class="shelfContainer">
-            <div class="stackSlider">
-              <Stack stackRef={`${shelfRef}`} />
-            </div>
-          </div>
-        {/* </ActiveStackProvider>
-      </StackDraggingProvider>
-    </SelectedBinderProvider> */}
+    <div class="shelfContainer">
+      <div class="stackSlider">
+        <Stack stackRef={`${shelfRef}`} />
+      </div>
+    </div>
   );
 }
