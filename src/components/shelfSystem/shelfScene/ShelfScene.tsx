2ba23c1aeed8ba6c81fe9f7cd3bfc8fbb6fc880b
The0inkinator <127580789+The0inkinator@users.noreply.github.com>
2023-09-01 09:43:14 -0700

    change shelfscene mapping to <For>

--- src/components/shelfSystem/shelfScene/ShelfScene.tsx
+++ src/components/shelfSystem/shelfScene/ShelfScene.tsx
@@ -31,9 +31,9 @@ export default function ShelfScene() {
         newShelf();
       }}
     >
-      {shelfList().map((component) => {
-        return component;
-      })}
+      <For each={shelfList()} fallback={<div>Loading stacks...</div>}>
+        {(item) => <>{item}</>}
+      </For>
     </div>
   );
 }
