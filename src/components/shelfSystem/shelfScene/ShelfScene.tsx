80c0e56ce80bcef5a15789ec0a5b1d60ca93ba99
The0inkinator <127580789+The0inkinator@users.noreply.github.com>
2023-09-08 15:28:13 -0700

    add initial version of newshelf calling

--- src/components/shelfSystem/shelfScene/ShelfScene.tsx
+++ src/components/shelfSystem/shelfScene/ShelfScene.tsx
@@ -1,9 +1,11 @@
 import './shelfSceneStyles.css';
 import Shelf from '../shelf/Shelf';
-import { createSignal, onMount, For } from 'solid-js';
+import { createSignal, createEffect, onMount, For } from 'solid-js';
+import { useStackStateContext } from '../../../context/StackStateContext';
 
 export default function ShelfScene() {
   const [shelfList, setShelfList] = createSignal<any[]>([]);
+  const [stackState, { queueStack }]: any = useStackStateContext();
 
   onMount(() => {
     setShelfList((prevList) => [
@@ -11,16 +13,38 @@ export default function ShelfScene() {
       <Shelf binderList="/colors" />,
       // <Shelf binderList="2" />,
     ]);
+
+    newShelfCheck();
   });
 
+  // createEffect(() => {
+  //   if (stackState().stackQueued !== 'none') {
+  //     newShelf(stackState().stackQueued);
+  //   }
+  // });
+
   function newShelf(path: string) {
-    setShelfList((prevList) => [
-      ...prevList,
-      () => {
-        return <Shelf binderList={`${path}`} />;
-      },
-    ]);
+    if (stackState().stackQueued !== 'none') {
+      setShelfList((prevList) => [
+        ...prevList,
+        () => {
+          return <Shelf binderList={`${path}`} />;
+        },
+      ]);
+    }
   }
+
+  const newShelfCheck = () => {
+    function loop() {
+      if (stackState().stackQueued === 'none') {
+        setTimeout(loop, 100);
+      } else {
+        // console.log('loop found stack Queued');
+        newShelf(stackState().stackQueued);
+      }
+    }
+    loop();
+  };
 
   return (
     <div>
